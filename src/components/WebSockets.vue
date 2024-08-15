0267c23e0580e5083e04d53595f8afc0cd1f1655
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-08-15 11:20:10 -0500

    hide url

--- src/components/WebSockets.vue
+++ src/components/WebSockets.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { io } from 'socket.io-client'
-const socket = io('ws://192.168.86.48:3000') 
+  const serverUrl = import.meta.env.VITE_APP_SOCKET_IO_SERVER_URL;
+  console.log(serverUrl)
+const socket = io(serverUrl) 
 socket.on('connect', () => {
     console.log('Connected to the Socket.IO server:', socket.id); // Socket ID is logged
   });
