627a7e03c5cc49d88c4ebd9408f0e6fa981a0819
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-08-15 15:36:04 -0500

    removed commented code

--- src/components/WebSockets.vue
+++ src/components/WebSockets.vue
@@ -1,15 +1,8 @@
 <script setup>
-// import { io } from 'socket.io-client'
 import { watch } from 'vue'
 import { useRecordingStore } from '../stores/recordingStore'
 const store = useRecordingStore()
 import { socket }from '../socket'
-
-// const serverUrl = import.meta.env.VITE_APP_SOCKET_IO_SERVER_URL;
-// const socket = io(serverUrl) 
-// socket.on('connect', () => {
-//     console.log('Connected to the Socket.IO server:', socket.id); // Socket ID is logged
-// });
 
 socket.on('videoUrl', (url) => {
 console.log(url)
