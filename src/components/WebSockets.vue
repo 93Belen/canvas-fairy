9d054b3c02d2d5c9ae85ac52d30b5d22cdbef4c5
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-08-15 11:24:59 -0500

    nothing really

--- src/components/WebSockets.vue
+++ src/components/WebSockets.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { io } from 'socket.io-client'
-  const serverUrl = import.meta.env.VITE_APP_SOCKET_IO_SERVER_URL;
-  console.log(serverUrl)
+
+const serverUrl = import.meta.env.VITE_APP_SOCKET_IO_SERVER_URL;
 const socket = io(serverUrl) 
 socket.on('connect', () => {
     console.log('Connected to the Socket.IO server:', socket.id); // Socket ID is logged
-  });
+});
 
 </script>
 
