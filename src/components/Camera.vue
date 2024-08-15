c33453b4aa7af7ca0b59fe788fc127b10b9874cb
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-08-15 16:52:33 -0500

    styling

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -3,13 +3,15 @@ import { onMounted, ref } from 'vue'
 import { useRecordingStore } from '../stores/recordingStore'
 const store = useRecordingStore()
 
-let video;
+let video, canvas;
 let recording = ref(false)
 let mediaRecorder;
 let videoChucks = []
+let timeOut = ref(5)
 
 onMounted(() => {
   video = document.getElementById("video");
+  canvas = document.getElementById("canvas")
 
   video.width = window.innerWidth
   video.height = window.innerHeight
@@ -27,7 +29,8 @@ onMounted(() => {
 
 
       //  Record video
-      mediaRecorder = new MediaRecorder(stream)
+      const videoStream = canvas.captureStream(30)
+      mediaRecorder = new MediaRecorder(videoStream)
       mediaRecorder.ondataavailable = (event) => {
         if(event.data.size > 0){
           videoChucks.push(event.data)
@@ -54,15 +57,29 @@ window.addEventListener('resize', () => {
 // Start and Stop Recording
 const record = () => {
   if(recording.value){
+      store.clearVideoBlob()
+      mediaRecorder.stop()
+      recording.value = false
+  }
+  else {
+    recording.value = true
+    let interval = setInterval(() => {
+        timeOut.value--
+    }, 1000)
+    setTimeout(() => {
+      clearInterval(interval)
+      timeOut.value = 5
+      store.clearVideoBlob()
+      mediaRecorder.start()
+    }, 5000)
+  }
+  setTimeout(() => {
+    clearInterval(interval)
+    timeOut.value = 5
     store.clearVideoBlob()
     mediaRecorder.stop()
     recording.value = false
-  }
-  else {
-    store.clearVideoBlob()
-    mediaRecorder.start()
-    recording.value = true
-  }
+  }, 60000)
 }
 
 
@@ -71,22 +88,53 @@ const record = () => {
 </script>
 
 <template>
-    <video src="" id="video"></video>
-    <button :style="recording ? 'background: red' : 'background: green' " @click="record">{{ recording ? 'Stop' : 'Record'}}</button>
+  <p v-if="timeOut !== 5">{{ timeOut }}</p>
+  <video src="" id="video"></video>
+  <button :class="recording ? 'recording' : 'not-recording'" @click="record">
+    {{ recording ? '' : 'Record' }}
+  </button>
 </template>
 
 <style scoped>
 video {
-    display: none;
+  display: none;
 }
+
 button {
   position: absolute;
-  bottom: 150px;
-  left: 150px;
-  width: 100px;
+  bottom: 50px;
+  left: 50px;
   height: 30px;
   border-radius: 50px;
   border: none;
   color: white;
+  transition: width 0.3s ease;
+}
+
+button.recording {
+  background: red;
+  width: 30px;
+  animation: parpadea 1s ease-in-out infinite;
+}
+
+button.not-recording {
+  background: green;
+  width: 100px;
+}
+
+p {
+  position: absolute;
+  font-size: 50px;
+  color: black;
+  left: 50vw;
+}
+
+@keyframes parpadea {
+  0% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
